fc2991a5191aa06ac449ad18004c6ceb297bcf3a
Arturo <Arturo.f.iglesias@gmail.com>
2012-07-03 01:35:23 -0300

    clase nodo

---
+++ teo2012/src/teo2012/Node.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teo2012;
+
+/**
+ *
+ * @author arturoiglesias
+ */
+public class Node {
+    
+    private Node left;
+    private Node right;
+    private String symbol;
+    private double value;
+    
+    
+    public Node (Node l, Node r, String s, double v){
+        left=l;
+        right=r;
+        symbol=s;
+        value=v;
+    }
+    
+    public void setLeft(Node l){
+        left=l;
+    }
+    
+    public void setRight(Node r){
+        right=r;
+    }
+    
+    public void setSymbol(String s){
+        symbol=s;
+    }
+    
+    public void setValue(double v){
+        value=v;
+    }
+    
+    public Node getLeft(){
+        return left;
+    }
+    
+    public Node getRight(){
+        return right;
+    }
+    
+    public String getSymbol(){
+        return symbol;
+    }
+    
+    public double getValue(){
+        return value;
+    }
+}
