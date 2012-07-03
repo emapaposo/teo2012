263ade6f89aaf5cd439ebfcb2fbba1df98cfeec8
Arturo <Arturo.f.iglesias@gmail.com>
2012-07-03 01:36:01 -0300

    clase huffman modificada

--- teo2012/src/teo2012/Huffman.java
+++ teo2012/src/teo2012/Huffman.java
@@ -9,5 +9,27 @@ package teo2012;
  * @author emn
  */
 public class Huffman {
+ 
+    /**
+    * 
+    */
     
+            
+    public void huffmanTree(){
+        // Ordenar la lista de porbabilidades
+        // agarro el ultimo, valor =0
+        // agarro el anteultimo, valor =1
+        // los sumo
+        // agrego a la lista 
+        // reordeno la lista
+        //
+    }
+
+    /**
+    * 
+    */
+    public void unDoIt(){
+    
+    }
+
 }
